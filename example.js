12713220b940a7a0cfd27e782aed92eaa442a94b
Wesley Whitney <wesleywhitney@gmail.com>
2016-09-14 22:17:20 -0600

    I like turtles

--- example.js
+++ example.js
@@ -1,9 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  if(req.url == '/kittens/') {
-    res.end('Kittens! Yay!');
-}
+    if(req.url == '/kittens/') {
+        res.end('Kittens! Yay!');
+    }
+    if(req.url == '/turtles/') {
+        res.end('I like turtles');
+    }
+
 });
 
 var port = 3040;
